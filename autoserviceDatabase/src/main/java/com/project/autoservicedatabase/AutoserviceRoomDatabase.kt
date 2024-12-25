696f8cd6c358707f747f42b1a456e92b9665e4f5
nkoro <nkorolev090@gmail.com>
2024-12-25 09:40:01 +0300

    add client data 2

--- autoserviceDatabase/src/main/java/com/project/autoservicedatabase/AutoserviceRoomDatabase.kt
+++ autoserviceDatabase/src/main/java/com/project/autoservicedatabase/AutoserviceRoomDatabase.kt
@@ -5,18 +5,24 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.project.autoservicedatabase.dao.ClientDao
 import com.project.autoservicedatabase.dao.UserDataDao
+import com.project.autoservicedatabase.models.ClientDBO
 import com.project.autoservicedatabase.models.UserDataDBO
 
 class AutoserviceDatabase internal constructor(private val database: AutoserviceRoomDatabase){
     val userDataDao: UserDataDao
         get() = database.userDataDao()
+
+    val clientDao: ClientDao
+        get() = database.clientDao()
 }
-@Database(entities = [UserDataDBO::class], version = 1)
+@Database(entities = [UserDataDBO::class, ClientDBO::class], version = 2)
 @TypeConverters(LocalDateTimeConverter::class)
 internal abstract class AutoserviceRoomDatabase : RoomDatabase() {
 
     abstract fun userDataDao(): UserDataDao
+    abstract fun clientDao(): ClientDao
 
 }
 fun AutoserviceDatabase(applicationContext: Context): AutoserviceDatabase {
@@ -25,6 +31,8 @@ fun AutoserviceDatabase(applicationContext: Context): AutoserviceDatabase {
         checkNotNull(applicationContext.applicationContext),
         AutoserviceRoomDatabase::class.java,
         "autoservice"
-    ).build()
+    )
+            .fallbackToDestructiveMigration()
+            .build()
     return AutoserviceDatabase(autoserviceRoomDatabase)
 }
--- autoserviceDatabase/src/main/java/com/project/autoservicedatabase/dao/ClientDao.kt
+++ autoserviceDatabase/src/main/java/com/project/autoservicedatabase/dao/ClientDao.kt
@@ -1,4 +1,17 @@
 package com.project.autoservicedatabase.dao
 
-class ClientDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.project.autoservicedatabase.models.ClientDBO
+
+@Dao
+interface ClientDao {
+    @Query("SELECT * FROM client WHERE id = :id")
+    suspend fun get(id: Int): List<ClientDBO>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(clientDBO: ClientDBO)
+
 }
